6dd5910f60a3e6747611cc1cbdc736c85bb9f1f6
duanbowen <dbwencookie@gmail.com>
2018-03-03 16:03:58 +0800

    modify 用户界面显示

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Layout, Menu, Avatar, Modal } from 'antd';
+import { Layout, Menu, Avatar } from 'antd';
 import { connect } from 'dva';
 import Rank from './Rank';
 import Comments from './Comment/Comments';
